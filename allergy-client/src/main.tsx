7c8f7dd0f516d2d61cc301db3ffb73d68f5883a4
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-05-20 17:40:37 +1200

    Configure layer order Use GlobalStyles component from mui -Pass the enableCssLayer prop to the StyledEngineProvider component. ref: https://mui.com/material-ui/integrations/tailwindcss/tailwindcss-v4/#vite-js-or-any-other-spa

--- allergy-client/src/main.tsx
+++ allergy-client/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StyledEngineProvider } from '@mui/material/styles';
+import GlobalStyles from '@mui/material/GlobalStyles';
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StyledEngineProvider enableCssLayer>
+      <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+      <App />
+    </StyledEngineProvider>
   </StrictMode>,
-)
+);
