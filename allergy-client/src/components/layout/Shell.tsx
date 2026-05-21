9876efb085eb37468b81b9404dfb8a58c47a3b23
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-05-21 19:05:24 +1200

    App layout

--- allergy-client/src/components/layout/Shell.tsx
+++ allergy-client/src/components/layout/Shell.tsx
@@ -1,7 +1,41 @@
-const Shell = () => {
-  return (
-    <div>Shell</div>
-  )
-}
+import { Outlet } from "react-router-dom";
+import { useTheme,
+    AppBar,
+    Toolbar,
+    Typography,
+    BottomNavigation,
+    BottomNavigationAction,
+ } from "@mui/material";
+import {
+    Home,
+    BarChart,
+    Settings,
+} from "@mui/icons-material";
 
-export default Shell
+const Shell = () => {
+  const theme = useTheme();
+  return (
+    <div
+      className="flex flex-col mx-auto h-dvh max-w-97.5" // 97.5 = 390px
+      style={{ backgroundColor: theme.palette.background.default }}
+    >
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Allergy Tracker</Typography>
+        </Toolbar>
+      </AppBar>
+      
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      
+      <BottomNavigation>
+        <BottomNavigationAction label="Home" icon={<Home />} />
+        <BottomNavigationAction label="Trends" icon={<BarChart />} />
+        <BottomNavigationAction label="Settings" icon={<Settings />} />
+      </BottomNavigation>
+    </div>
+  );
+};
+
+export default Shell;
