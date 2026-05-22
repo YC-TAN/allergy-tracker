1158594022f71f37ef8fada004200d8339563109
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-05-22 19:13:49 +1200

    Add SymptomCard Component

--- allergy-client/src/pages/HomePage.tsx
+++ allergy-client/src/pages/HomePage.tsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 
+import { SeverityRating, type Symptom, type SeverityRatingType } from "../schemas";
 import SeverityCard from "../components/ui/SeverityCard";
-import { SeverityRating, type SeverityRatingType } from "../schemas";
+import SymptomCard from "../components/ui/SymptomCard";
 
 const HomePage = () => {
   const [severity, setSeverity] = useState<SeverityRatingType>(SeverityRating.Mild)
+  const [symptoms, setSymptoms] = useState<Symptom[]>([]);
   return (
     <div>
       HomePage
       <SeverityCard severity={severity} setSeverity={setSeverity} />
+      <SymptomCard symptoms={symptoms} setSymptoms={setSymptoms}/>
     </div>
   )
 }
