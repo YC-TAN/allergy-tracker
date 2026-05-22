be28016fbe443aa8e5864ccb2f5193e65d6d48c3
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-05-22 18:38:52 +1200

    Add Severity Card

--- allergy-client/src/pages/HomePage.tsx
+++ allergy-client/src/pages/HomePage.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
+
+import SeverityCard from "../components/ui/SeverityCard";
+import { SeverityRating, type SeverityRatingType } from "../schemas";
+
 const HomePage = () => {
+  const [severity, setSeverity] = useState<SeverityRatingType>(SeverityRating.Mild)
   return (
-    <div>HomePage</div>
+    <div>
+      HomePage
+      <SeverityCard severity={severity} setSeverity={setSeverity} />
+    </div>
   )
 }
 
