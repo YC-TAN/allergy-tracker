d9618917019e29c2f9f69cb28487560b33ffaf6d
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-05-22 20:12:45 +1200

    Add NotesCard component and submit button

--- allergy-client/src/pages/HomePage.tsx
+++ allergy-client/src/pages/HomePage.tsx
@@ -3,15 +3,33 @@ import { useState } from "react";
 import { SeverityRating, type Symptom, type SeverityRatingType } from "../schemas";
 import SeverityCard from "../components/ui/SeverityCard";
 import SymptomCard from "../components/ui/SymptomCard";
+import NotesCard from "../components/ui/NotesCard";
+
+import {Button} from '@mui/material'
 
 const HomePage = () => {
   const [severity, setSeverity] = useState<SeverityRatingType>(SeverityRating.Mild)
   const [symptoms, setSymptoms] = useState<Symptom[]>([]);
+  const [notes, setNotes] = useState<string | null>(null);
+
+  const handleSubmit = () => {
+
+  }
+
   return (
     <div>
       HomePage
       <SeverityCard severity={severity} setSeverity={setSeverity} />
       <SymptomCard symptoms={symptoms} setSymptoms={setSymptoms}/>
+      <NotesCard notes={notes} setNotes={setNotes} />
+      <Button
+        variant="contained"
+        color="primary"
+        fullWidth
+        onClick={handleSubmit}
+      >
+        Save entry
+      </Button>
     </div>
   )
 }
