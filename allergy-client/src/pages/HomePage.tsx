fc43e580c547a566ddb068d3c7e6b84b184c1509
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-05-25 18:40:59 +1200

    Add CheckIn component to Homepage

--- allergy-client/src/pages/HomePage.tsx
+++ allergy-client/src/pages/HomePage.tsx
@@ -1,9 +1,20 @@
 import LogForm from "../components/log/LogForm";
+import CheckIn from "../components/log/CheckIn";
 
 const HomePage = () => {
 
+  const handleAllGood = () => {
+
+  }
+
+  const handleLogSymptoms = () => {
+    
+  }
+
+
   return (
     <div>
+      <CheckIn onAllGood={handleAllGood} onLogSymptoms={handleLogSymptoms}/>
       <LogForm />
     </div>
   )
