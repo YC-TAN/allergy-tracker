698fc59fd7c158036666c99441e8f2534225feab
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-05-29 09:37:31 +1200

    Update schema and labels

--- allergy-client/src/schemas/index.ts
+++ allergy-client/src/schemas/index.ts
@@ -15,13 +15,18 @@ export const SeverityRating = {
   Severe: 3,
 } as const;
 
-export const SeveritySchema = z.union([
+export const SeverityInputSchema = z.union([
   z.literal(SeverityRating.Mild),
   z.literal(SeverityRating.Moderate),
   z.literal(SeverityRating.Severe),
 ]);
 
-export type SeverityRatingType = z.infer<typeof SeveritySchema>;
+export type SeverityRatingType = z.infer<typeof SeverityInputSchema>;
+
+export const SeveritySchema = z.union([
+  ...SeverityInputSchema.options,
+  z.literal(SeverityRating.NoSymptom)
+])
 
 export const SymptomSchema = z.enum(['eyes', 'nose', 'throat', 'energy', 'headache', 'other']);
 export type Symptom = z.infer<typeof SymptomSchema>;
