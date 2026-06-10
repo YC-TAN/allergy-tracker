01b415484d5c0ad2b0c3dde59cabbd752f7824b8
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-06-10 20:13:15 +1200

    Update SeverityLabel to include no symptom

--- allergy-client/src/schemas/labels.ts
+++ allergy-client/src/schemas/labels.ts
@@ -1,12 +1,13 @@
 /**
  * Map of `SeverityRating` values to human-readable labels.
  *
- * Used to display a user-friendly severity string for entry form.
+ * Used to display a user-friendly severity string.
  */
 
 import { SeverityRating, type SeverityRatingType } from "./index";
 
 export const SeverityLabel: Record<SeverityRatingType, string> = {
+  [SeverityRating.NoSymptom]: "No Symptom",
   [SeverityRating.Mild]: "Mild",
   [SeverityRating.Moderate]: "Moderate",
   [SeverityRating.Severe]: "Severe",
