73ffcac8896b75e85fd67c15993b470418e8d665
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-06-16 20:45:59 +1200

    Inject mockdata for dev

--- allergy-client/src/main.tsx
+++ allergy-client/src/main.tsx
@@ -11,6 +11,11 @@ import GlobalStyles from "@mui/material/GlobalStyles";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 
+import { seedMockEntries } from './utils/seedData'
+if (import.meta.env.DEV) {
+  seedMockEntries()
+}
+
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
