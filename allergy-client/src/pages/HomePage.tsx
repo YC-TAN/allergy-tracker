4ace308ae0f3c97268df97161ce860207c573cf0
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-06-18 18:30:51 +1200

    Add TomorrowCard component - To display weather forecast and pollen risk

--- allergy-client/src/pages/HomePage.tsx
+++ allergy-client/src/pages/HomePage.tsx
@@ -8,16 +8,18 @@ import { useEntry } from "../hooks/useEntry";
 
 import CheckIn from "../components/entry/CheckIn";
 import DailyLog from "../components/entry/DailyLog"
+import TomorrowCard from "../components/entry/TomorrowCard";
 
 const HomePage = () => {
   const { entry, isPending } = useEntry();
 
   if (isPending) return <div>loading...</div>
-  if (entry) return <DailyLog />
+  // if (entry) return <DailyLog />
 
   return (
-    <div>
-      <CheckIn />
+    <div className="flex flex-col items-center px-5 pt-8 pb-6">
+      {entry ? (<DailyLog />) : (<CheckIn />)}
+      <TomorrowCard />
     </div>
   )
 }
