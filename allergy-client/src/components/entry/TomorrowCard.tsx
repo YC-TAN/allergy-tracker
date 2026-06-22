2db2123bf1fb5317b43b33936b6c4b52693ac109
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-06-22 20:46:12 +1200

    Added mock data to forecast

--- allergy-client/src/components/entry/TomorrowCard.tsx
+++ allergy-client/src/components/entry/TomorrowCard.tsx
@@ -1,9 +1,28 @@
-import { Card, CardContent } from "@mui/material";
+import { Card, CardContent, Typography } from "@mui/material";
 
 const TomorrowCard = () => {
+
+  const forecast = {
+    pollenRisk: "Low",
+    condition: "Fine apart from morning fog or frost. High cloud developing in the evening. Light winds."
+  }
   return (
-    <Card>
-      <CardContent>TomorrowCard</CardContent>
+    <Card 
+      className="w-full mb-6"
+      sx={{
+        backgroundColor: "#f0f7f0",
+        // border: "1px solid #a5d6a7",
+      }}
+    >
+      <CardContent>
+        <Typography variant="body1" gutterBottom>
+          Tomorrow's Conditions 
+          <br />
+          {forecast.condition}
+          <br />
+          Pollen risk: {forecast.pollenRisk}
+        </Typography>
+      </CardContent>
     </Card>
   );
 };
