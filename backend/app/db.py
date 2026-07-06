7642f6bf9c2cab825c3d6eea1bf78954bb8cac13
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-07-06 12:26:30 +1200

    Update docstrings

--- backend/app/db.py
+++ backend/app/db.py
@@ -6,11 +6,13 @@ from app.config import get_settings
 @lru_cache
 def get_supabase() -> Client:
     """
-    Using @lru_cache to ensure that the client is instantiated only once 
-    and the app is talking to the same client instance with same shared state and configuration.
+    Instantiate Supabase Client exactly once using @lru_cache, 
+    to be used as dependency so it is swappable in tests.
+   
+    Caching and reusing the same instance allows the underlying libraries maintain open connections to Supabase's servers, 
+    ensuring the app is talking to the same client instance with same shared state and configuration.
     
-    Reusing the same instance allows the underlying libraries maintain open connections to Supabase's servers.
-    Using secret key which bypass the RLS by design for server process.
+    Secret key bypass the RLS, which is designed for server process.
     """
     settings = get_settings()
     return create_client(settings.supabase_url, settings.supabase_key)
