e55c2f53f36bd0682f3f79f8aabb8499f9b917e2
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-07-13 20:16:01 +1200

    Modify PollenForecastBase to mirror API response

--- backend/app/schemas/pollen_forecast.py
+++ backend/app/schemas/pollen_forecast.py
@@ -1,18 +1,25 @@
-from sqlmodel import SQLModel, Field, Column
-from sqlalchemy.dialects.postgresql import ARRAY
-from sqlalchemy import String
-from typing import Literal
+"""
+13/Jul/26 Schema Update
+Goal    - To capture plant list by their respective pollen level. 
+Reason  - To closely mirror the JSON returned by source API
+How     - Plant list will be stored in db using JSON, with its level as field name
+Adopting ELT (Extract, Load, Transform), collect first, transform later 
+"""
+
+from sqlmodel import SQLModel, Field
+from sqlalchemy import JSON
+from typing import Literal, List
 from datetime import date as date_type, datetime
 from uuid import UUID, uuid4
-from enum import Enum
 
-PollenRisk = Literal["low", "moderate", "high", "imminent", "unknown"]
+PollenRisk = Literal["low", "moderate", "high", "imminent"]
 
 class PollenForecastBase(SQLModel):
     date: date_type
     location: str
-    pollen_info: str                                     
-    risk: PollenRisk
+    imminent: List[str] = Field(default=[], sa_type=JSON)
+    low: List[str] = Field(default=[], sa_type=JSON)
+    high: List[str] = Field(default=[], sa_type=JSON)
     checked_at: datetime
 
 class PollenForecast(PollenForecastBase, table=True):
