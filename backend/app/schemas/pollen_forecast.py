e5c23c4f505b91518ea60b7ecf264f2feb3706ae
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-07-14 20:11:42 +1200

    Move checked_at to model schema - generate utc time from server

--- backend/app/schemas/pollen_forecast.py
+++ backend/app/schemas/pollen_forecast.py
@@ -2,29 +2,27 @@
 13/Jul/26 Schema Update
 Goal    - To capture plant list by their respective pollen level. 
 Reason  - To closely mirror the JSON returned by source API
-How     - Plant list will be stored in db using JSON, with its level as field name
+How     - Plant list will be stored in db using JSONB, with pollen level as field name
 Adopting ELT (Extract, Load, Transform), collect first, transform later 
 """
 
 from sqlmodel import SQLModel, Field
-from sqlalchemy import JSON
-from typing import Literal, List
-from datetime import date as date_type, datetime
+from sqlalchemy import JSONB
+from typing import List
+from datetime import date as date_type, datetime, timezone
 from uuid import UUID, uuid4
-
-PollenRisk = Literal["low", "moderate", "high", "imminent"]
 
 class PollenForecastBase(SQLModel):
     date: date_type
     location: str
-    imminent: List[str] = Field(default=[], sa_type=JSON)
-    low: List[str] = Field(default=[], sa_type=JSON)
-    high: List[str] = Field(default=[], sa_type=JSON)
-    checked_at: datetime
+    imminent: List[str] = Field(default=[], sa_type=JSONB)
+    low: List[str] = Field(default=[], sa_type=JSONB)
+    high: List[str] = Field(default=[], sa_type=JSONB)
 
 class PollenForecast(PollenForecastBase, table=True):
     __tablename__ = "daily_pollen_forecasts"
     id: UUID = Field(default_factory=uuid4, primary_key=True)
+    checked_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
 class PollenForecastPublic(PollenForecastBase):
     pass
