1ed8e23e96067869ec4ba4a07d584a43de2b85ba
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-07-29 21:18:47 +1200

    Add routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
+from .routers import pollen_forecast, internal
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.include_router(pollen_forecast.router)
+app.include_router(internal.router)
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def main():
     print("Hello from backend!")
