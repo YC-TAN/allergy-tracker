dad2cb120cdfef1b05966915417872a8bca4904c
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-08-05 19:38:02 +1200

    Create Entry schema

--- backend/app/schemas/entry.py
+++ backend/app/schemas/entry.py
@@ -1,21 +1,58 @@
+from enum import Enum
 from sqlmodel import SQLModel, Field
 from sqlalchemy.dialects.postgresql import JSONB
 from datetime import date as date_type, datetime, timezone
 from uuid import UUID, uuid4
 
 
+class SeverityLevel(int, Enum):
+    no_symptom = 0
+    mild = 1
+    moderate = 2
+    severe = 3
+
+
+class Symptom(str, Enum):
+    eyes = "eyes"
+    nose = "nose"
+    throat = "throat"
+    energy = "energy"
+    headache = "headache"
+    other = "other"
+
+
 class EntryBase(SQLModel):
     date: date_type
-    severity: str
-    symptoms: list[str] = Field(default=[], sa_type=JSONB)
-    notes: str | None = Field(default=None)
+    severity: SeverityLevel
+    #TODO deduplicate the list - prevent storing duplicate symptoms
+    symptoms: list[Symptom] = Field(
+        default=[], 
+        sa_type=JSONB,
+        title="Hay Fever Symptoms",
+        description="Multiselect: 'eyes', 'nose', 'throat', 'energy', 'headache', 'other'"
+    )
+    notes: str | None = Field(
+        default=None,
+        max_length=255,
+        title="Additional information"
+    )
 
 
 class Entry(EntryBase, table=True):
     __tablename__ = "entries"
     id: UUID = Field(default_factory=uuid4, primary_key=True)
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
 
 class EntryCreate(EntryBase):
     pass
+
+
+class EntryUpdate(EntryBase):
+    pass
+
+
+class EntryResponse(EntryBase):
+    pass
+
