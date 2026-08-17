ec1bdcdfaec1cfd2f1e14199e92a87f808b1c8d4
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-08-17 17:16:03 +1200

    Add EntryWithID type

--- allergy-client/src/schemas/index.ts
+++ allergy-client/src/schemas/index.ts
@@ -72,6 +72,12 @@ export type Entry = z.infer<typeof EntrySchema>;
 // To allow fields with default value to be optional in forms, can be omitted before parsing
 export type EntryInput = z.input<typeof EntrySchema>;
 
+export const EntryWithIDSchema = EntrySchema.extend({
+  id: z.uuid(),
+})
+
+export type EntryWithID = z.infer<typeof EntryWithIDSchema>;
+
 // ---- Settings
 export const SettingsSchema = z.object({
   notify_time: z.string().regex(/^\d{2}:\d{2}$/).default('20:00'), // HH:MM, 8pm
