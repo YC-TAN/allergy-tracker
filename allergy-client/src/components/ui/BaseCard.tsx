797c37f603952df587a794052f03a514c73d43c0
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-09-10 18:36:00 +1200

    Refactor CardTitle

--- allergy-client/src/components/ui/BaseCard.tsx
+++ allergy-client/src/components/ui/BaseCard.tsx
@@ -1,34 +1,18 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import { 
+  Card, 
+  CardContent, 
+} from "@mui/material";
 
 interface BaseCardProps extends React.HTMLAttributes<HTMLDivElement> {
-  cardHeader: string;
   children: React.ReactNode;
-  optional: boolean;
 }
 
 const BaseCard = ({
-  cardHeader,
   children,
-  optional = false,
 }: BaseCardProps) => {
   return (
-    <Card className="mb-4 w-full">
+    <Card className="mb-2 w-full">
       <CardContent>
-        <Typography variant="subtitle1" className="block mb-2.5">
-          {cardHeader + " "}
-          {optional && (
-            <Typography
-              component="span"
-              variant="subtitle1"
-              sx={{
-                fontWeight: 400,
-                textTransform: 'none',
-              }}
-            >
-              (optional)
-            </Typography>
-          )}
-        </Typography>
         {children}
       </CardContent>
     </Card>
