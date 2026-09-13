2f39ca2ddf685c046e94757358d7fcd2d1ab34e3
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-09-14 10:54:33 +1200

    Update Typography lineHeight

--- allergy-client/src/theme/index.ts
+++ allergy-client/src/theme/index.ts
@@ -150,12 +150,11 @@ const theme = createTheme({
       // textTransform: "uppercase",
     },
     body1: {
-      // fontSize:   '1rem',
-      lineHeight: 1.6,
+      lineHeight: 2,
     },
     body2: {
       fontSize: "0.875rem",
-      lineHeight: 1.55,
+      lineHeight: 1.5,
     },
     caption: {
       fontSize: "0.875rem",
