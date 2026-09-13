3cbedb1d812ad5d19423525c935aa9545120c178
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-09-14 10:51:06 +1200

    Update BaseCard to accept style

--- allergy-client/src/components/ui/BaseCard.tsx
+++ allergy-client/src/components/ui/BaseCard.tsx
@@ -1,20 +1,28 @@
-import { 
-  Card, 
-  CardContent, 
-} from "@mui/material";
+import { Card, CardContent, type SxProps, type Theme } from "@mui/material";
 
 interface BaseCardProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
+  sx?: SxProps<Theme>;
+  backgroundColor?: string;
 }
 
 const BaseCard = ({
   children,
+  sx,
+  backgroundColor,
+  ...props
 }: BaseCardProps) => {
   return (
-    <Card className="mb-2 w-full">
-      <CardContent>
-        {children}
-      </CardContent>
+    <Card
+      // variant={variant}
+      className="mb-3 w-full"
+      sx={{
+        backgroundColor: backgroundColor || "background.paper",
+        ...sx
+      }}
+      {...props}
+    >
+      <CardContent>{children}</CardContent>
     </Card>
   );
 };
