ab900f705b9932f85480440c47eb00b59a032f26
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-09-14 11:21:40 +1200

    Rename path to match component

--- allergy-client/src/router.tsx
+++ allergy-client/src/router.tsx
@@ -8,7 +8,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Shell from "./components/layout/Shell";
 import HomePage from "./pages/HomePage";
-import LogPage from "./pages/EntryFormPage";
+import EntryFormPage from "./pages/EntryFormPage";
 import TrendsPage from "./pages/TrendsPage";
 import SettingsPage from "./pages/SettingsPage";
 import NotFoundPage from "./pages/NotFoundPage";
@@ -19,8 +19,8 @@ export const router = createBrowserRouter([
     element: <Shell />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: "log", element: <LogPage /> },
-      { path: "log/:date", element: <LogPage /> },
+      { path: "entry", element: <EntryFormPage /> },
+      { path: "entry/:date", element: <EntryFormPage /> },
       { path: "trends", element: <TrendsPage /> },
       { path: "settings", element: <SettingsPage /> },
       { path: "login", element: <LoginPage /> },
