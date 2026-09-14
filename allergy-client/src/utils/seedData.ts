a5ed43599377e3339c6eab7d6ffdc1e2b416df1c
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-09-14 22:06:30 +1200

    Update seed to always load previous 7 days

--- allergy-client/src/utils/seedData.ts
+++ allergy-client/src/utils/seedData.ts
@@ -1,24 +1,40 @@
 import { EntrySchema } from "../schemas";
+import { getRelativeDays } from "./dates";
 import { saveEntry, clearAllEntries } from "./storage";
 
 const MOCK_ENTRIES = [
-  { date: '2026-08-28', severity: 0, symptoms: [], notes: '' },
-  { date: '2026-08-29', severity: 1, symptoms: ['nose'], notes: '' },
-  { date: '2026-08-30', severity: 2, symptoms: ['eyes', 'nose', 'throat'], notes: 'Windy' },
-  { date: '2026-08-31', severity: 3, symptoms: ['eyes', 'nose', 'headache'], notes: 'Drying clothes indoor' },
-  { date: '2026-09-01', severity: 1, symptoms: ['nose'], notes: '' },
-  { date: '2026-06-16', severity: 0, symptoms: [], notes: '' },
+  { severity: 0, symptoms: [], notes: "" },
+  { severity: 1, symptoms: ["nose"], notes: "" },
+  { severity: 2, symptoms: ["eyes", "nose", "throat"], notes: "Windy" },
+  {
+    severity: 0,
+    symptoms: ["eyes", "nose", "headache"],
+    notes: "Drying clothes indoor",
+  },
+  { severity: 1, symptoms: ["nose"], notes: "" },
+  { severity: 3, symptoms: [], notes: "" },
 ] as const;
 
 export const seedMockEntries = () => {
-    MOCK_ENTRIES.forEach((data) => {
-        const entry = EntrySchema.parse(data);
-        saveEntry(entry);
-    })
-    console.log('dev mock data seeded', MOCK_ENTRIES.length, 'entries to localStorage');
-}
+  const previous7days = getRelativeDays(MOCK_ENTRIES.length, false);
+
+  const entries = MOCK_ENTRIES.map((entry, index) => ({
+    date: previous7days[index],
+    ...entry,
+  }));
+  entries.forEach((e) => {
+    const entry = EntrySchema.parse(e);
+    saveEntry(entry);
+  });
+  console.log(
+    "dev mock data seeded",
+    MOCK_ENTRIES.length,
+    "entries to localStorage",
+    entries
+  );
+};
 
 export const resetMockEntries = () => {
   clearAllEntries();
   seedMockEntries();
-}
+};
